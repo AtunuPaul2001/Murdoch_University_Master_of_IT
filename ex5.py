8b953456dc25654bdfbf6ad10b9b73d137f6ea9c
Atunu Paul <atunupaul2001@gmail.com>
2026-03-13 00:12:37 +0800

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,24 @@
+# Use the formula Celsius=(Fahrenheit-32)x5/9 to convert several temperature values from Fahrenheit to Celsius.
+# Note, each time, you must store the temperature in Fahrenheit in a variable
+# and then use the above formula to calculate its value in Celsius
+# and print the value out.
+# Note, this program must be named ex5.py and stored in the folder lab01.
+
+fahrenheit = 98.6
+celsius = ((fahrenheit-32)*5) / 9
+print("Fahrenheit", fahrenheit)
+print ("Celsius", celsius)
+
+print()
+
+fahrenheit = 100
+celsius = ((fahrenheit-32)*5) / 9
+print("Fahrenheit", fahrenheit)
+print ("Celsius", celsius)
+
+print()
+
+fahrenheit = 50
+celsius = ((fahrenheit-32)*5) / 9
+print("Fahrenheit", fahrenheit)
+print ("Celsius", celsius)
