383eec40bc3cf106c608dd52c9b5ad74aa2a4c42
Atunu Paul <atunupaul2001@gmail.com>
2026-03-14 17:40:40 +0800

    Add files via upload

---
+++ Semester_1/Python/ex8.py
@@ -0,0 +1,8 @@
+# 8. List three different operators that are all represented by the same symbol + in Python.
+# Explain the applicable scope for each of these operators and give one example for each of them.
+
+print(1+2) #Basic arithmatic operation
+
+print("Hello" + "World") #Concatination of 2 strings
+
+print([1,2,3] + [4,5,6]) #Concatination of 2 list
